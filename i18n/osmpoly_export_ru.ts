d49365e9c13f398a2c1bee33989845d7003c7b2b
Maxim Dubinin <sim@gis-lab.info>
2016-03-08 22:19:51 +0300

    i18n

---
+++ i18n/osmpoly_export_ru.ts
@@ -0,0 +1,149 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0">
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <location filename="aboutdialog.py" line="54"/>
+        <source>Version: %s</source>
+        <translation>Версия: %s</translation>
+    </message>
+    <message>
+        <location filename="aboutdialog.py" line="55"/>
+        <source>Export OSM Poly</source>
+        <translation>Экспорт в OSM Poly</translation>
+    </message>
+    <message>
+        <location filename="aboutdialog.py" line="81"/>
+        <source>&lt;p&gt;This plugin exports vector polygons in OSM Poly format.&lt;/p&gt;&lt;p&gt;Its export all or selected set of polygons and names output files according to table of attributes.&lt;p&gt;&lt;strong&gt;Developer&lt;/strong&gt;: Maxim Dubinin (&lt;a href=&quot;http://nextgis.com&quot;&gt;NextGIS&lt;/a&gt;).&lt;/p&gt;&lt;p&gt;&lt;strong&gt;Homepage&lt;/strong&gt;: &lt;a href=&quot;https://github.com/nextgis/osmpoly_export&quot;&gt;https://github.com/nextgis/osmpoly_export&lt;/a&gt;&lt;/p&gt;&lt;p&gt;Please report bugs at &lt;a href=&quot;https://github.com/nextgis/osmpoly_export/issues&quot;&gt;bugtracker&lt;/a&gt;&lt;/p&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Dialog</name>
+    <message>
+        <location filename="ui/aboutdialogbase.ui" line="20"/>
+        <source>logo</source>
+        <translation>логотип</translation>
+    </message>
+    <message>
+        <location filename="ui/aboutdialogbase.ui" line="47"/>
+        <source>version</source>
+        <translation>версия</translation>
+    </message>
+    <message>
+        <location filename="ui/aboutdialogbase.ui" line="70"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Ubuntu&apos;; font-size:11pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;Sans Serif&apos;; font-size:9pt;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Ubuntu&apos;; font-size:11pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;Sans Serif&apos;; font-size:9pt;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="ui/aboutdialogbase.ui" line="14"/>
+        <source>About Export to OSM Poly</source>
+        <translation>О расширении</translation>
+    </message>
+    <message>
+        <location filename="ui/aboutdialogbase.ui" line="37"/>
+        <source>Export to OSM Poly</source>
+        <translation>Экспорт в OSM Poly</translation>
+    </message>
+</context>
+<context>
+    <name>osmpoly_export</name>
+    <message>
+        <location filename="polygenerator.py" line="69"/>
+        <source>Error</source>
+        <translation>Ошибка</translation>
+    </message>
+    <message>
+        <location filename="polygenerator.py" line="69"/>
+        <source>QGIS %s detected.
+</source>
+        <translation>Обнаружен QGIS %s
+</translation>
+    </message>
+    <message>
+        <location filename="polygenerator.py" line="69"/>
+        <source>This version of OSMInfo requires at least QGIS version 2.0.
+Plugin will not be enabled.</source>
+        <translation>Эта версия OSMInfo требует QGIS минимум версии 2.0.
+Плагин работать не будет.</translation>
+    </message>
+    <message>
+        <location filename="polygenerator.py" line="99"/>
+        <source>Export OSM Poly</source>
+        <translation type="unfinished">Экспорт в OSM Poly</translation>
+    </message>
+    <message>
+        <location filename="polygenerator.py" line="77"/>
+        <source>Start conversion to Poly</source>
+        <translation>Запустить преобразование в формат Poly</translation>
+    </message>
+    <message>
+        <location filename="polygenerator.py" line="78"/>
+        <source>Export vector polygons to poly-files</source>
+        <translation>Экспорт полигонов в файлы poly</translation>
+    </message>
+    <message>
+        <location filename="polygenerator.py" line="80"/>
+        <source>About</source>
+        <translation>О расширении</translation>
+    </message>
+    <message>
+        <location filename="polygenerator.py" line="82"/>
+        <source>About OSMPoly_export</source>
+        <translation>О расширении</translation>
+    </message>
+    <message>
+        <location filename="polygenerator.py" line="110"/>
+        <source>Warning</source>
+        <translation>Внимание</translation>
+    </message>
+    <message>
+        <location filename="polygenerator.py" line="111"/>
+        <source>Information</source>
+        <translation>Информация</translation>
+    </message>
+    <message>
+        <location filename="polygenerator.py" line="113"/>
+        <source>No layers selected</source>
+        <translation>Не выбраны слои</translation>
+    </message>
+    <message>
+        <location filename="polygenerator.py" line="117"/>
+        <source>Not a vector layer</source>
+        <translation>Не выбраны векторные слои</translation>
+    </message>
+    <message>
+        <location filename="polygenerator.py" line="121"/>
+        <source>Not a polygon layer</source>
+        <translation>Это не полигональный слой</translation>
+    </message>
+    <message>
+        <location filename="polygenerator.py" line="125"/>
+        <source>Using %s selected features</source>
+        <translation>Будет сконвертировано %s объект(ов)</translation>
+    </message>
+    <message>
+        <location filename="polygenerator.py" line="129"/>
+        <source>No features selected, using all %s features</source>
+        <translation>Объекты не выбраны, будут сконвертированы все имеющиеся</translation>
+    </message>
+    <message>
+        <location filename="polygenerator.py" line="140"/>
+        <source>No string field names. Exiting</source>
+        <translation>Нет строковых полей в таблице.</translation>
+    </message>
+    <message>
+        <location filename="polygenerator.py" line="151"/>
+        <source>Choose a folder</source>
+        <translation>Выберите папку</translation>
+    </message>
+</context>
+</TS>
